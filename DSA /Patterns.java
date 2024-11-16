4f0adf473b50e4608222c52fba0a481820085d7c
Muneeb <muneebsmacbook@Muneebs-MacBook-Pro.local>
2024-11-16 19:58:27 +0500

    Version 1 restored

--- DSA /Patterns.java
+++ DSA /Patterns.java
@@ -1,6 +1,6 @@
 public class Patterns {
 
     public static void main(String[] args) {
-        System.out.println("Patterns in Commit 2");
+        System.out.println("Version 1 restored");
     }
 }
