a414b210de8c74c1e48b27f3206dd562ea63c8bb
Muneeb <muneebsmacbook@Muneebs-MacBook-Pro.local>
2024-11-18 16:55:44 +0500

    Pattern 11

--- DSA /Patterns.java
+++ DSA /Patterns.java
@@ -1,6 +1,25 @@
 public class Patterns {
-
+    
+    public void  p11(int n){
+        for (int i = 0; i < n; i++){
+            int start = 0;
+            if (i % 2 == 0){
+                start = 1;
+            }
+            for (int j = 0; j < i; j++){
+                System.out.print(start);
+                start = (start == 0) ? 1 : 0;         
+            }
+            System.out.println();   
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Version 1 restored");
+        Patterns obj = new Patterns();
+
+
+        obj.p11(5);
+
+        
+        
     }
 }
